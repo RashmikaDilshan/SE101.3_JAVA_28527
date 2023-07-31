84895e8baf7330de7e93094d7af69b94546c3d7c
RashmikaDilshan <dilshanrdv@gmail.com>
2023-07-31 21:44:32 +0530

    Added Lab10 practical Answers

---
+++ Lab10/Q1/Calculate.java
@@ -0,0 +1,11 @@
+package Lab10.Q1;
+
+public class Calculate {
+  public static float divide(float a, float b) throws ArithmeticException {
+    if (b == 0) {
+      throw new ArithmeticException();
+    }
+
+    return a / b;
+  }
+}
---
+++ Lab10/Q1/Main.java
@@ -0,0 +1,24 @@
+package Lab10.Q1;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    float a, b;
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the number 1 : ");
+    a = sc.nextFloat();
+
+    System.out.print("Enter the number 2 : ");
+    b = sc.nextFloat();
+
+    try {
+      float result = Calculate.divide(a, b);
+      System.out.printf("%.0f/%.0f = %.2f\n", a, b, result);
+    } catch (ArithmeticException e) {
+      System.out.println("\nCannot divide by zero");
+    } finally {
+      sc.close();
+    }
+  }
+}
