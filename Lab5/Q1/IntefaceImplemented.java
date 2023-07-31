edb08ed6ad8109f4bbc6def912f034232399c8b8
RashmikaDilshan <dilshanrdv@gmail.com>
2023-07-31 21:41:52 +0530

    Added Lab5 practical Answers

---
+++ Lab5/Q1/IntefaceImplemented.java
@@ -0,0 +1,6 @@
+package Lab6.Q1;
+
+public class IntefaceImplemented implements MyFirstInterface {
+  @Override
+  public void display() {}
+}
---
+++ Lab5/Q1/MyFirstInterface.java
@@ -0,0 +1,7 @@
+package Lab6.Q1;
+
+public interface MyFirstInterface {
+  int x = 0;
+
+  void display();
+}
